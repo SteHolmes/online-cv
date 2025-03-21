29f29e3c809dadfacb58f6f2fd8897e86fae844f
Stephen Holmes <Stephen.Holmes@qualsafe.org>
2025-03-21 11:28:31 +0000

    Map logo data to render LogoGrid items. Update images and adjust animation sequences to improve performance.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -9,7 +9,7 @@ export default function NotFound() {
           <h2>404: Not Found</h2>
           <div className="contentCard dualColumnLayout dualColumnLayout--centered">
             <div>
-              <Image className="imageContent" src="https://images.unsplash.com/photo-1721622560183-4172c8ef8934?q=80&w=2030&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width={100} height={150} alt="Unknown image chosen at random" />
+              <Image className="imageContent" src="/images/cool-cat.png" width={600} height={840} alt="Image of a cat wearing sunglasses" />
             </div>
             <div>
               <h3>Could not find the requested resource</h3>
--- components/LogoGrid/LogoGrid.tsx
+++ components/LogoGrid/LogoGrid.tsx
@@ -1,51 +1,62 @@
 import Image from 'next/image'
 import styles from './LogoGrid.module.scss'
 
-const LogoGrid = function() {
+export default function LogoGrid() {
+  const rootDirectory = '/logos/'
+  
+  const logos = [
+    {
+      src: `${rootDirectory}html.png`,
+      label: 'HTML',
+    },
+    {
+      src: `${rootDirectory}css.png`,
+      label: 'CSS',
+    },
+    {
+      src: `${rootDirectory}javascript.png`,
+      label: 'Javascript',
+    },
+    {
+      src: `${rootDirectory}sass.png`,
+      label: 'SASS',
+    },
+    {
+      src: `${rootDirectory}git.png`,
+      label: 'Git',
+    },
+    {
+      src: `${rootDirectory}vue-logo.png`,
+      label: 'Vue',
+    },
+    {
+      src: `${rootDirectory}nuxt.png`,
+      label: 'Nuxt',
+    },
+    {
+      src: `${rootDirectory}react.png`,
+      label: 'React',
+    },
+    {
+      src: `${rootDirectory}next.png`,
+      label: 'Next',
+    },
+    {
+      src: `${rootDirectory}express.png`,
+      label: 'Express',
+    }
+  ]
+
+  const renderedLogos = logos.map((logo) => {
+    return <div key={logo.label}>
+      <Image src={logo.src} alt={`${logo.label} logo`} width={100} height={100} />
+      <div className="skills__item-name">{logo.label.toUpperCase()}</div>
+    </div>
+  })
+
   return (
-      <div className={styles.logoGrid}>
-        <div className="">
-          <Image src="/logos/html.png" alt="" width={100} height={100} />
-          <div className="skills__item-name">HTML</div>
-        </div>
-        <div className="">
-          <Image src="/logos/css.png" alt="" width={100} height={100} />
-          <div className="skills__item-name">CSS</div>
-        </div>
-        <div className="">
-          <Image src="/logos/javascript.png" alt="" width={100} height={100} />
-          <div className="skills__item-name">JAVASCRIPT</div>
-        </div>
-        <div className="">
-          <Image src="/logos/sass.png" alt="" width={100} height={100} />
-          <div className="skills__item-name">SASS</div>
-        </div>
-        <div className="">
-          <Image src="/logos/git.png" alt="" width={100} height={100} />
-          <div className="skills__item-name">GIT</div>
-        </div>
-        <div className="">
-          <Image src="/logos/vue.png" alt="" width={100} height={100} />
-          <div className="skills__item-name">VUE.JS</div>
-        </div>
-        <div className="">
-          <Image src="/logos/nuxt.png" alt="" width={100} height={100} />
-          <div className="skills__item-name">NUXT</div>
-        </div>
-        <div className="">
-          <Image src="/logos/react.png" alt="" width={100} height={100} />
-          <div className="skills__item-name">REACT</div>
-        </div>
-        <div className="">
-          <Image src="/logos/next.png" alt="" width={100} height={100} />
-          <div className="skills__item-name">NEXT.JS</div>
-        </div>
-        <div className="">
-          <Image src="/logos/node.png" alt="" width={100} height={100} />
-          <div className="skills__item-name">NODE.JS</div>
-        </div>
-      </div>
+    <div className={styles.logoGrid}>
+      { renderedLogos }
+    </div>
   )
 }
-
-export default LogoGrid
