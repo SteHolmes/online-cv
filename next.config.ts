51152277f087f173ded224e6162259220d23199a
Stephen Holmes <Stephen.Holmes@qualsafe.org>
2025-03-21 13:35:20 +0000

    Add namespaces to style imports. Small styling adjustments. Remove unused code.

--- next.config.ts
+++ next.config.ts
@@ -11,10 +11,6 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
       },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
     ],
   },
 };
